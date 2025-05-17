1d7e627e50a91fd96171d5eaa16dea411f4a1fab
Muhammad <m.shaafan@outlook.de>
2025-05-17 09:12:35 +0200

    test

---
+++ js/models/movable-object.class.js
@@ -0,0 +1,22 @@
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+
+    //loadImage('img/test.png);
+    loadImage(path){
+        this.img = new Image(); // this.img = document.getElementById('image')<img id="image">
+        this.img.src = path
+    }
+
+
+
+     moveRight() {
+        console.log('Moving right')
+    }
+
+       moveLeft() {
+        
+    }
+}
