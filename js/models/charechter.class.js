4b00f44a4c3d738ccacd6bdeeb2793306b146976
Muhammad <m.shaafan@outlook.de>
2025-05-17 19:01:15 +0200

    Chicken animieren

--- js/models/charechter.class.js
+++ js/models/charechter.class.js
@@ -10,7 +10,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
-    currentImage = 0;
+  
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -26,7 +26,7 @@ class Character extends MovableObject {
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
-        }, 1000);
+        }, 100);
     }
 
 
