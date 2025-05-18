4d32cb3ff6573c2c6b45d1dc18df05e07d80a5b8
Muhammad <m.shaafan@outlook.de>
2025-05-18 08:37:18 +0200

    Bild spiegeln

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -9,13 +9,13 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-         new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 0)
-       
+
         //
-        
+
     ]
     canvas;
     ctx;
@@ -43,7 +43,7 @@ class World {
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
-         
+
 
 
         //draw() wird immer wieder aufgerufen
@@ -61,6 +61,15 @@ class World {
     }
 
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.img.width, 0);
+            this.ctx.scale(-1, 1);
+        }
+
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            this.ctx.restore();
+        }
     }
 }
