eb32b8bd9cc5f76b2d2836993981de2771add699
Muhammad <m.shaafan@outlook.de>
2025-05-18 13:08:53 +0200

    Character optimieren

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,8 +1,6 @@
 class World {
     character = new Character();
-    enemies = level1.enemies;
-    clouds = level1.clouds;
-    backgroundObjects = level1.backgroundObjects;
+    level = level1;
     canvas;
     ctx;
     keyboard;
@@ -27,12 +25,12 @@ class World {
 
         this.ctx.translate(this.camera_x, 0);
 
-        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.enemies);
-        
-          this.ctx.translate(-this.camera_x, 0);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
 
         //draw() wird immer wieder aufgerufen
         let self = this;
