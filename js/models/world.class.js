3f1504bc5f3e91392f810495983a3b30f8754657
Muhammad <m.shaafan@outlook.de>
2025-05-19 22:31:27 +0200

    Statusbar 5

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -28,7 +28,7 @@ class World {
             this.level.enemies.forEach( (enemy) => {
                if( this.character.isColliding(enemy) ) {
                 this.character.hit();
-                 console.log('collision with Character, energy ', this.character.energy);
+                 this.statusBar.setPercentage(this.character.energy);
 
                }
             })
@@ -40,7 +40,10 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.backgroundObjects);
+         this.ctx.translate(-this.camera_x, 0);
+         // -------------- Space for fixed objects -------------
         this.addToMap(this.statusBar);
+         this.ctx.translate(this.camera_x, 0);
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
