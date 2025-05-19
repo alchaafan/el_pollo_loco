c908120c3d9955a299e6f380a15524d8263ad07b
Muhammad <m.shaafan@outlook.de>
2025-05-19 17:20:23 +0200

    isHurt()

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -11,6 +11,7 @@ class MovableObject {
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
+    lastHit = 0;
 
 
 
@@ -61,7 +62,15 @@ class MovableObject {
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
         }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit; // Differenz in ms
+        timepassed = timepassed / 1000; // Differenz in Sekunden
+        return timepassed < 1;
     }
 
     isDead() {
@@ -79,7 +88,7 @@ class MovableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
