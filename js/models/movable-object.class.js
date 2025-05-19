1fa294c2d1e5ee52901d3e5f0f337a0543c644ef
Muhammad <m.shaafan@outlook.de>
2025-05-19 08:16:04 +0200

    Gravitation implementieren

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -8,6 +8,24 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+
+
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround(){
+        return this.y < 180;
+    }
 
 
     //loadImage('img/test.png);
@@ -27,10 +45,10 @@ class MovableObject {
     }
 
     playAnimation(images) {
-             let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = images[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
     }
 
 
